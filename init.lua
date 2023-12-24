9f25306f8e45440802e35626cca509ab3988bfb0
Boken <b@boken.org>
2023-12-24 11:14:34 -0500

    added autopairs

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ require('editor')
 require('key-maps')
 
 -- Plugin configurations
+require('plugins.nvim-autopairs')
 require('plugins.nvim-tree')
 require('plugins.nvim-lspconfig')
---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,7 @@
+require('nvim-autopairs').setup({
+	map_char = {
+		all = '(',
+		tex = '{',
+	},
+	enable_check_bracket_line = false,
+})
