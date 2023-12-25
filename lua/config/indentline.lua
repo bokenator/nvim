495ac6c1b846404ca0151d72a02a5ba11252b3a4
Boken <b@boken.org>
2023-12-25 17:56:24 -0500

    added indentline

--- lua/config/indentline.lua
+++ lua/config/indentline.lua
@@ -7,9 +7,4 @@ require('ibl').setup({
 	scope = {
 		char = icons.ui.LineMiddle,
 	},
-	--context_char = icons.ui.LineMiddle,
-	--show_trailing_blankline_indent = false,
-	--show_first_indent_level = true,
-	--use_treesitter = true,
-	--show_current_context = true,
 })
