a2b010fe733dcfd6394c6e68a76cc174035b54da
Boken <b@boken.org>
2023-12-25 19:32:44 -0500

    added tab bar and status bar

---
+++ lua/config/statusline.lua
@@ -0,0 +1,31 @@
+require('lualine').setup({
+	options = {
+		component_separators = { left = '', right = ''},
+	    section_separators = { left = '', right = ''},
+		ignore_focus = { 'NvimTree' },
+		globalstatus = false,
+		disabled_filetypes = {
+			statusline = { 'NvimTree' },
+			winbar = { 'NvimTree' },
+		},
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff' },
+		lualine_c = { 'filename', 'location' },
+		lualine_x = { 'diagnostics' },
+		lualine_y = { 'filetype' },
+		lualine_z = { 'progress' },
+	},
+	winbar = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'buffers'},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	extensions = {
+		'nvim-tree',
+	},
+})
