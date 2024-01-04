adc46089f46589a30beaf79faa90fd36a0843628
Boken <b@boken.org>
2024-01-04 11:58:15 -0500

    added scrollbar and bufferline to update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 git subtree pull --prefix pack/plugins/start/autopairs https://github.com/windwp/nvim-autopairs.git master --squash
 git subtree pull --prefix pack/plugins/start/bufdelete https://github.com/famiu/bufdelete.nvim.git master --squash
+git subtree pull --prefix pack/plugins/start/bufferline https://github.com/akinsho/bufferline.nvim.git main --squash
 git subtree pull --prefix pack/plugins/start/cmp https://github.com/hrsh7th/nvim-cmp.git main --squash
 git subtree pull --prefix pack/plugins/start/cmp-buffer https://github.com/hrsh7th/cmp-buffer.git main --squash
 git subtree pull --prefix pack/plugins/start/cmp-cmdline https://github.com/hrsh7th/cmp-cmdline.git main --squash
@@ -15,6 +16,7 @@ git subtree pull --prefix pack/plugins/start/mason https://github.com/williambom
 git subtree pull --prefix pack/plugins/start/mason-lspconfig https://github.com/williamboman/mason-lspconfig.nvim.git main --squash
 git subtree pull --prefix pack/plugins/start/lspconfig https://github.com/neovim/nvim-lspconfig.git master --squash
 git subtree pull --prefix pack/plugins/start/rust https://github.com/rust-lang/rust.vim.git master --squash
+git subtree pull --prefix pack/plugins/start/scrollbar https://github.com/petertriho/nvim-scrollbar.git main --squash
 git subtree pull --prefix pack/plugins/start/statusline https://github.com/nvim-lualine/lualine.nvim.git master --squash
 git subtree pull --prefix pack/plugins/start/tree https://github.com/nvim-tree/nvim-tree.lua.git master --squash
 git subtree pull --prefix pack/plugins/start/treesitter https://github.com/nvim-treesitter/nvim-treesitter.git master --squash
