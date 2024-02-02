228785e399f1d5d9eeaafa25b382f476905a8de7
Boken <b@boken.org>
2024-02-02 15:44:06 -0500

    removed telescope

--- update.sh
+++ update.sh
@@ -16,11 +16,9 @@ git subtree pull --prefix pack/plugins/start/indentline https://github.com/lukas
 git subtree pull --prefix pack/plugins/start/mason https://github.com/williamboman/mason.nvim.git main --squash
 git subtree pull --prefix pack/plugins/start/mason-lspconfig https://github.com/williamboman/mason-lspconfig.nvim.git main --squash
 git subtree pull --prefix pack/plugins/start/lspconfig https://github.com/neovim/nvim-lspconfig.git master --squash
-git subtree pull --prefix pack/plugins/start/plenary https://github.com/nvim-lua/plenary.nvim.git master --squash
 git subtree pull --prefix pack/plugins/start/rust https://github.com/rust-lang/rust.vim.git master --squash
 git subtree pull --prefix pack/plugins/start/scrollbar https://github.com/petertriho/nvim-scrollbar.git main --squash
 git subtree pull --prefix pack/plugins/start/statusline https://github.com/nvim-lualine/lualine.nvim.git master --squash
-git subtree pull --prefix pack/plugins/start/telescope https://github.com/nvim-telescope/telescope.nvim.git master --squash
 git subtree pull --prefix pack/plugins/start/tree https://github.com/nvim-tree/nvim-tree.lua.git master --squash
 git subtree pull --prefix pack/plugins/start/treesitter https://github.com/nvim-treesitter/nvim-treesitter.git master --squash
 git subtree pull --prefix pack/plugins/start/treesitter-textobjects https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git master --squash
