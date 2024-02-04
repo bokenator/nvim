e4ef3ecacb2c8231164d4226f5b8dce7c7d8573a
Boken <b@boken.org>
2024-02-03 21:31:26 -0500

    using searchV instead of search for multi-cursor

--- lua/config/multicursor.lua
+++ lua/config/multicursor.lua
@@ -13,7 +13,7 @@ vim.keymap.set({'n', 'i'}, '<C-Down>', '<cmd>MultipleCursorsAddDown<CR>', option
 vim.keymap.set('n', '<C-j>', '<cmd>MultipleCursorsAddDown<CR>', options)
 vim.keymap.set({'n', 'i'}, '<C-Up>', '<cmd>MultipleCursorsAddUp<CR>', options)
 vim.keymap.set('n', '<C-k>', '<cmd>MultipleCursorsAddUp<CR>', options)
-vim.keymap.set({'n', 'i'}, '<C-LeftMouse>', '<cmd>MultipleCursorsAddDelete<CR>', options)
-vim.keymap.set({'n', 'x'}, '<Leader>a', '<cmd>MultipleCursorsAddBySearch<CR>', options)
-vim.keymap.set({'n', 'x'}, '<Leader>A', '<cmd>MultipleCursorsAddBySearch<CR>', options)
+vim.keymap.set({'n', 'i'}, '<C-LeftMouse>', '<cmd>MultipleCursorsMouseAddDelete<CR>', options)
+vim.keymap.set({'n', 'x'}, '<Leader>a', '<cmd>MultipleCursorsAddBySearchV<CR>', options)
+vim.keymap.set({'n', 'x'}, '<Leader>A', '<cmd>MultipleCursorsAddBySearchV<CR>', options)
 
